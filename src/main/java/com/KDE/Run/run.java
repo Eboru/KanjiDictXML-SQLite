c2df062b54e87220f3d6115dd638d911bec5f232
Aldo Iván Garza González <garzagonzalez_08@hotmail.com>
2020-11-11 22:56:20 -0600

    Polished

--- src/main/java/com/KDE/Run/run.java
+++ src/main/java/com/KDE/Run/run.java
@@ -1,4 +1,4 @@
-package com.KDE.run;
+package com.KDE.Run;
 
 import com.KDE.KanjiDic.Dictionary;
 import com.KDE.KanjiDic.DBGen;
@@ -14,10 +14,11 @@ public class run {
         if(args.length<2)
         {
             System.err.println("NOT ENOUGH ARGUMENTS!");
-            System.err.println("USE \"thisJar.jar <KanjiDictXMLFile> <Database output directory>\"");
+            System.err.println("USE \"thisJar.jar <KanjiDictXMLFile> <Database output file)>\"");
             return;
         }
-        
+
+        args[1]+="\\kanjidic2.db";
         File file  = new File(args[1]);
         if(file.length() !=0)
         {
@@ -26,9 +27,13 @@ public class run {
         }
 
 
-        Dictionary dic = new Dictionary(args[1]);
+        System.out.println("STARTING BUILDING DATABASE...");
+
+        Dictionary dic = new Dictionary(args[0]);
         DBGen dbGen = new DBGen();
-        dbGen.genFromKanjiDic(dic, DBGen.DBType.SQLite, args[2], "");
+        dbGen.genFromKanjiDic(dic, DBGen.DBType.SQLite, args[1], "");
+
+        System.out.println("BUILDING DONE!");
 
     }
 }
